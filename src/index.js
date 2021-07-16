1c5032aff2996965aba671a3b20a32813a4d518a
Yusuf <66310246+xnamespace@users.noreply.github.com>
2021-07-16 23:45:13 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    DataBase: require('./structures/DataBase'),
-};
--- src/types/replaceAll.js
+++
@@ -1,5 +0,0 @@
-String.prototype.replaceAll = (find, replace) => {
-    const str = this;
-
-    return str.replace(new RegExp(find, 'g'), replace);
-}
